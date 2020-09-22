ccdcb22f31eac862192f6984458d0493f4bea68f
Maruf <marufbdonline@gmail.com>
2020-09-23 05:58:02 +0600

    Added IPaymentMethod Repository

---
+++ ExpenseTracker/ExpenseTracker.Framework/Repositories/IPaymentMethodRepository.cs
@@ -0,0 +1,14 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Framework.Contexts;
+using ExpenseTracker.Framework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Repositories
+{
+    public interface IPaymentMethodRepository : IRepository<PaymentMethod, int, FrameworkContext>
+    {
+
+    }
+}
