03c07b6488f999356a96da97f5d3e850a5261b0f
Maruf <marufbdonline@gmail.com>
2020-09-23 05:41:34 +0600

    Added Payment Method entity

---
+++ ExpenseTracker/ExpenseTracker.Framework/Entities/PaymentMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExpenseTracker.Data;
+
+namespace ExpenseTracker.Framework.Entities
+{
+    public class PaymentMethod : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
