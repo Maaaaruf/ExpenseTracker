6f5965eb9bb77ad97bdfabf3417af35210ff37fd
Maruf <marufbdonline@gmail.com>
2020-09-22 23:20:10 +0600

    Added a dbContext

---
+++ ExpenseTracker/ExpenseTracker.Framework/Contexts/FrameworkContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Contexts
+{
+    public class FrameworkContext : DbContext
+    {
+        private string _connectionString;
+        private string _migrationAssemblyName;
+
+        public FrameworkContext(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+    }
+}
