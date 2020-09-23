c02ebbf373e6658706ebd3b89a47c56d6d9242ca
Maruf <marufbdonline@gmail.com>
2020-09-23 20:37:09 +0600

    Working Index and Added Create method

---
+++ ExpenseTracker/ExpenseTracker.Web/Areas/User/Controllers/PaymentMethodController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using ExpenseTracker.Web.Areas.User.Models.PaymentMethods;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Web.Areas.User.Controllers
+{
+    public class PaymentMethodController : Controller
+    {
+        [Area("User")]
+        public IActionResult Index()
+        {
+            var model = Startup.AutofacContainer.Resolve<PaymentMethodBaseModel>();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            var model = Startup.AutofacContainer.Resolve<PaymentMethodBaseModel>();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CreatePaymentMethodModel model)
+        {
+            
+            return View(model);
+        }
+    }
+}
