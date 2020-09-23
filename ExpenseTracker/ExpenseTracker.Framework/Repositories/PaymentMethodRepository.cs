4bad9f8342e40be4c6ebc4bcec3a3098d0e70499
Maruf <marufbdonline@gmail.com>
2020-09-23 06:02:31 +0600

    Added PaymentMethodRepository

---
+++ ExpenseTracker/ExpenseTracker.Framework/Repositories/PaymentMethodRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Framework.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Repositories
+{
+    public class PaymentMethodRepository : Repository<PaymentMethod, int, DbContext>
+    {
+        public PaymentMethodRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
