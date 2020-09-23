40bb99bd8d8ae02d1d93beedb530a4839efbe3a5
Maruf <marufbdonline@gmail.com>
2020-09-23 06:55:28 +0600

    Added User Area

--- ExpenseTracker/ExpenseTracker.Web/Startup.cs
+++ ExpenseTracker/ExpenseTracker.Web/Startup.cs
@@ -84,8 +84,12 @@ namespace ExpenseTracker.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                
                 endpoints.MapRazorPages();
             });
         }
