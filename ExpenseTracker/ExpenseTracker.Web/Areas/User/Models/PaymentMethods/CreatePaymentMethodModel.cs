8da71efd67fe78a4bce44192a00212a187ec8f55
Maruf <marufbdonline@gmail.com>
2020-09-23 21:07:28 +0600

    Added Create Method

--- ExpenseTracker/ExpenseTracker.Web/Areas/User/Models/PaymentMethods/CreatePaymentMethodModel.cs
+++ ExpenseTracker/ExpenseTracker.Web/Areas/User/Models/PaymentMethods/CreatePaymentMethodModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExpenseTracker.Framework.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ namespace ExpenseTracker.Web.Areas.User.Models.PaymentMethods
 {
     public class CreatePaymentMethodModel : PaymentMethodBaseModel
     {
+        public string Name { get; set; }
 
+        public void Create()
+        {
+            var paymentMethod = new PaymentMethod { Name = Name };
+            _paymentMethodService.Create(paymentMethod);
+        }
     }
 }
