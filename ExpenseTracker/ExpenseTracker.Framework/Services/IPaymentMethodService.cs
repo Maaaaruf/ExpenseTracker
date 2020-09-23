980bbdbee906b10f05252890d8fa0b5b09982973
Maruf <marufbdonline@gmail.com>
2020-09-23 06:52:49 +0600

    Added Crud Method

--- ExpenseTracker/ExpenseTracker.Framework/Services/IPaymentMethodService.cs
+++ ExpenseTracker/ExpenseTracker.Framework/Services/IPaymentMethodService.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using ExpenseTracker.Framework.Entities;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ExpenseTracker.Framework.Services
 {
-    public class IPaymentMethodService
+    public interface IPaymentMethodService
     {
-
+        void Create(PaymentMethod paymentMethod);
+        void Update(PaymentMethod paymentMethod);
+        void Remove(PaymentMethod paymentMethod);
+        void GetAll();
     }
 }
--- ExpenseTracker/ExpenseTracker.Framework/Services/PaymentMethodService.cs
+++ ExpenseTracker/ExpenseTracker.Framework/Services/PaymentMethodService.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using ExpenseTracker.Framework.Entities;
+using ExpenseTracker.Framework.Exceptions;
+using ExpenseTracker.Framework.UnitOfWorks;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ExpenseTracker.Framework.Services
 {
-    public class PaymentMethodService
+    public class PaymentMethodService : IPaymentMethodService
     {
+        public IExpenseUnitOfWork _expenseUnitOfWork { get; set; }
+
+        public PaymentMethodService(IExpenseUnitOfWork expenseUnitOfWork)
+        {
+            _expenseUnitOfWork = expenseUnitOfWork;
+        }
+
+        public void Create(PaymentMethod paymentMethod)
+        {
+            int count = _expenseUnitOfWork.PaymentMethodRepository.GetCount(x=>x.Name == paymentMethod.Name);
+            
+            if (count > 0)
+                throw new DuplicationException("Repeated item found", paymentMethod.Name);
+            else
+                _expenseUnitOfWork.PaymentMethodRepository.Add(paymentMethod);
+        }
+
+        public void Update(PaymentMethod paymentMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(PaymentMethod paymentMethod)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
