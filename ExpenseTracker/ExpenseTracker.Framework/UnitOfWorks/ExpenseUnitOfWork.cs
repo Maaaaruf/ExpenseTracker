a1ea4fa562d414dec0863545c728ba73deddcfda
Maruf <marufbdonline@gmail.com>
2020-09-23 06:03:52 +0600

    Added Expense UnitOfWork

---
+++ ExpenseTracker/ExpenseTracker.Framework/UnitOfWorks/ExpenseUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.UnitOfWorks
+{
+    public class ExpenseUnitOfWork : UnitOfWork
+    {
+        public ExpenseUnitOfWork(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Framework/UnitOfWorks/IExpenseUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using ExpenseTracker.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.UnitOfWorks
+{
+    public interface IExpenseUnitOfWork : IUnitOfWork
+    {
+
+    }
+}
