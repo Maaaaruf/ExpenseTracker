7c3abab885cf42ed74e385187b851bc7112ce24d
Maruf <marufbdonline@gmail.com>
2020-09-23 08:28:17 +0600

    Created Base Model with CreatePaymentmethodModel

---
+++ ExpenseTracker/ExpenseTracker.Web/Areas/User/Models/PaymentMethods/PaymentMethodBaseModel.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using ExpenseTracker.Framework.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Web.Areas.User.Models.PaymentMethods
+{
+    public class PaymentMethodBaseModel : UserBaseModel
+    {
+        public IPaymentMethodService _paymentMethodService;
+
+        public PaymentMethodBaseModel(IPaymentMethodService paymentMethodService)
+        {
+            _paymentMethodService = paymentMethodService;
+        }
+
+        public PaymentMethodBaseModel()
+        {
+            _paymentMethodService = Startup.AutofacContainer.Resolve<IPaymentMethodService>();
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Web/Areas/User/Models/UserBaseModel.cs
@@ -0,0 +1,35 @@
+﻿using ExpenseTracker.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Web.Areas.User.Models
+{
+    public class UserBaseModel
+    {
+        public MenuModel MenuModel { get; set; }
+
+        public UserBaseModel()
+        {
+            SetupMenu();
+        }
+
+        private void SetupMenu()
+        {
+            MenuModel = new MenuModel
+            {
+                MenuItems = new List<MenuItem>
+                {
+                    {
+                        new MenuItem
+                        {
+                            Title = "Books",
+                            URL = ""
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
