478b1cf22a7fb6d48e988f10c0e8fcec4f627c7f
Maruf <marufbdonline@gmail.com>
2020-09-23 08:01:46 +0600

    Inherited Interface

--- ExpenseTracker/ExpenseTracker.Framework/Repositories/PaymentMethodRepository.cs
+++ ExpenseTracker/ExpenseTracker.Framework/Repositories/PaymentMethodRepository.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace ExpenseTracker.Framework.Repositories
 {
-    public class PaymentMethodRepository : Repository<PaymentMethod, int, DbContext>
+    public class PaymentMethodRepository : Repository<PaymentMethod, int, DbContext>, IPaymentMethodRepository
     {
         public PaymentMethodRepository(DbContext context) : base(context)
         {
