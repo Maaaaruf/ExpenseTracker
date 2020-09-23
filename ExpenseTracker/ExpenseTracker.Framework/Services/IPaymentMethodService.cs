3046effcea22866037b7db48a49f63d649e1d990
Maruf <marufbdonline@gmail.com>
2020-09-23 06:13:14 +0600

    Added Payment Method Service

---
+++ ExpenseTracker/ExpenseTracker.Framework/Services/IPaymentMethodService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Services
+{
+    public class IPaymentMethodService
+    {
+
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker.Framework/Services/PaymentMethodService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Services
+{
+    public class PaymentMethodService
+    {
+    }
+}
