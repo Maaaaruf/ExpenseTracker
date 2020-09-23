cbc90dd6a8df5e5a19fbd52e0fd434c7a8a27d6b
Maruf <marufbdonline@gmail.com>
2020-09-23 07:38:28 +0600

    Added MenuItem

---
+++ ExpenseTracker/ExpenseTracker.Framework/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework
+{
+    public class MenuItem
+    {
+        public string Title { get; set; }
+        public string URL { get; set; }
+    }
+}
