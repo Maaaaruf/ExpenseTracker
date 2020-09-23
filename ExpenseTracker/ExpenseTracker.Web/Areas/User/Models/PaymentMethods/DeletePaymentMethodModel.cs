3724b59b54a8925c19ef01b26dcbc4a8b3fcfa8b
Maruf <marufbdonline@gmail.com>
2020-09-24 05:47:01 +0600

    Model for delete

---
+++ ExpenseTracker/ExpenseTracker.Web/Areas/User/Models/PaymentMethods/DeletePaymentMethodModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Web.Areas.User.Models.PaymentMethods
+{
+    public class DeletePaymentMethodModel : PaymentMethodBaseModel
+    {
+        public void delete(int id)
+        {
+            _paymentMethodService.Remove(id);
+        }
+    }
+}
