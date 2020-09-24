4bd3ae0b937e073a5af7116a08727bb9bcbd3364
Maruf <marufbdonline@gmail.com>
2020-09-24 06:50:46 +0600

    Applied Interface Segregation Principal

--- ExpenseTracker/ExpenseTracker.Framework/Services/IPaymentMethodService.cs
+++ ExpenseTracker/ExpenseTracker.Framework/Services/IPaymentMethodService.cs
@@ -1,16 +1,17 @@
 ﻿using ExpenseTracker.Framework.Entities;
+using ExpenseTracker.Framework.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ExpenseTracker.Framework.Services
 {
-    public interface IPaymentMethodService
+    public interface IPaymentMethodService : 
+        ICreateable<PaymentMethod>, 
+        IUpdateable<PaymentMethod>, 
+        IRemoveable<PaymentMethod>
     {
-        void Create(PaymentMethod paymentMethod);
-        void Update(PaymentMethod paymentMethod);
-        void Remove(PaymentMethod paymentMethod);
-        void Remove(int id);
+        
         (IList<PaymentMethod> records, int total, int totalFiltered) GetPaymentMethods(int pageIndex, int pageSize, string searchText, string sortText);
         PaymentMethod GetById(int id);
     }
---
+++ ExpenseTracker/ExpenseTracker.Framework/Services/IRemoveable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Services
+{
+    public interface IRemoveable<T>
+    {
+        void Remove(T entity);
+    }
+}
