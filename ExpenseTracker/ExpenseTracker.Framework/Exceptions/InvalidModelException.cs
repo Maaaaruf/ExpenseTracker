2affc691874b1b50c403c6584ebc4cade84ae682
Maruf <marufbdonline@gmail.com>
2020-09-24 06:26:40 +0600

    Added Invalid Model Exception

---
+++ ExpenseTracker/ExpenseTracker.Framework/Exceptions/InvalidModelException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Exceptions
+{
+    public class InvalidModelException : Exception
+    {
+        public InvalidModelException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
