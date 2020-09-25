0334cd877dec8503fca3e5da569f354797fd5ff9
maaaaruf <marufbdonline@gmail.com>
2020-09-25 18:46:38 +0600

    Configured XUnit Test

---
+++ ExpenseTracker/ExpenseTracker.Framework.Tests/Services/PaymentMethodServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ExpenseTracker.Framework.Tests.Services
+{
+    [ExcludeFromCodeCoverage]
+    public class PaymentMethodServiceTests : IDisposable
+    {
+        public PaymentMethodServiceTests()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
